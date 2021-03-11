581a732f7f58ccb65c42ab8e9ae1aa64c0ba52d2
Daniel Garcia Perea <dgarciap@choucairtesting.com>
2021-03-11 09:48:18 -0500

    Daniel

--- src/main/java/co/com/choucair/certification/Utest/userinterface/AddressPage.java
+++ src/main/java/co/com/choucair/certification/Utest/userinterface/AddressPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 
 public class AddressPage {
 
-    public static final Target INPUT_CITY = Target.the("location city").located(By.xpath("/html/body/ui-view/main/section/div/div[2]/div/div[2]/div/form/div[1]/div[3]/div[1]/div[1]/input"));
+    public static final Target INPUT_CITY = Target.the("location city").located(By.id("city"));
     public static final Target INPUT_ZIP = Target.the("location zip").located(By.id("zip"));
     public static final Target DIV_COU= Target.the("DIV PAIS").located(By.xpath("//*[@id='regs_container']/div/div[2]/div/div[2]/div/form/div[1]/div[3]/div[1]/div[4]/div[2]/div/div/div[1]"));
     public static final Target DIV_CIT= Target.the("DIV PAIS").located(By.xpath("//*[@id='regs_container']/div/div[2]/div/div[2]/div/form/div[1]/div[3]/div[1]/div[4]/div[2]/div/div/div[1]"));
